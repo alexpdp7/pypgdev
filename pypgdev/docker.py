61a9a1a231538c277630930ee800aae1c20928d0
alex <alex@pdp7.net>
2017-10-21 11:55:14 +0200

    Add start_db function and entry point

---
+++ pypgdev/docker.py
@@ -0,0 +1,34 @@
+import argparse
+import os
+from os import path
+import signal
+import subprocess
+
+
+def start_db(data_dir, name):
+    uid = os.getuid()
+    gid = os.getgid()
+    os.makedirs(data_dir, exist_ok=True)
+    abs_data_dir = path.abspath(data_dir)
+    command = ['docker',
+               'run',
+               '-i',
+               '--user', '{uid}:{gid}'.format(uid=uid, gid=gid),
+               '-v', '/etc/passwd:/etc/passwd:ro',
+               '-v', '{abs_data_dir}:/var/lib/postgresql/data'.format(abs_data_dir=abs_data_dir),
+               '--name', name,
+               'postgres',
+              ]
+    try:
+        subprocess.run(command, stdin=subprocess.PIPE, stdout=subprocess.PIPE, check=True)
+    finally:
+        subprocess.run(['docker', 'kill', name], check=True)
+        subprocess.run(['docker', 'rm', name], check=True)
+
+
+def start_db_main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_dir')
+    parser.add_argument('name')
+    args = parser.parse_args()
+    start_db(args.data_dir, args.name)
--- setup.py
+++ setup.py
@@ -5,4 +5,9 @@ setup(
     name='pypgdev',
     packages=find_packages(),
     install_requires=['ipython'],
+    entry_points={
+        'console_scripts': [
+            'pg_docker = pypgdev.docker:start_db_main'
+        ]
+    },
 )
